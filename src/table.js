3989b3e1cebd653e946261f5db192204be305ec1
陈成 <cheng@tanmer.com>
2021-03-04 16:30:50 +0800

    fix bug

--- src/table.js
+++ src/table.js
@@ -139,7 +139,7 @@ export class Table {
    * @returns {HTMLElement} - the area
    */
   _createContenteditableArea() {
-    return create('div', [CSS.inputField], {contenteditable: 'true'});
+    return create('div', [CSS.inputField], {contenteditable: !this.readOnly});
   }
 
   /**
